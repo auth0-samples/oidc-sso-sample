eebdbeddafb05e840573f3be068bcc1b3b66943e
Rodrigo López Dato <rodrigo@auth0.com>
2017-01-09 23:35:39 -0300

    Extract common auth0js object

--- auth0-variables.js
+++ auth0-variables.js
@@ -4,3 +4,12 @@ const AUTH0_DOMAIN = '';
 if (!AUTH0_CLIENT_ID || !AUTH0_DOMAIN) {
     alert('Make sure to set the AUTH0_CLIENT_ID and AUTH0_DOMAIN variables in auth0-variables.js.');
 }
+
+const auth0js = new auth0.WebAuth({
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    scope: 'openid profile',
+    responseType: 'token',
+    redirectUri: 'http://localhost:3000'
+});
--- callback.js
+++ callback.js
@@ -1,12 +1,3 @@
-const auth0js = new auth0.WebAuth({
-    domain: AUTH0_DOMAIN,
-    clientID: AUTH0_CLIENT_ID,
-    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
-    scope: 'openid profile',
-    responseType: 'token',
-    redirectUri: 'http://localhost:3000'
-});
-
 const authResponse = auth0js.parseHash(window.location.hash, function (err, response) {
     parent.postMessage(err || response, 'http://localhost:3000');
 });
